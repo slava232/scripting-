3d1a13ce1f7530b1ccd2b29770bd3bfbe6368880
slava232 <149590635+slava232@users.noreply.github.com>
2023-11-01 17:14:58 +0400

    Change

---
+++ HelloWorld1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вводит в консоль сообщение 
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
