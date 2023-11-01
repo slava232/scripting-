f16f05abd3a68851bd93d7001b41a11fc7015690
slava232 <149590635+slava232@users.noreply.github.com>
2023-11-01 17:38:46 +0400

    Add files via upload

---
+++ Destroy1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Уничтожение объекта
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
